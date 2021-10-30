5a656dd15ada711faaa7a21077e30d2ed8c85d7f
Andre121193 <andre1993andre@hotmail.com>
2021-10-30 11:31:34 -0300

    aprendendo a deletar usando MySQL

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+//==============Delete==============
+	
+			st = conn.prepareStatement(
+					"DELETE FROM seller "
+					+ "WHERE "
+					+ "Id = ?");
+
+			st.setInt(1, 8);
+			
+//==============Confirm it has been added==============
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+}
